d8daca707e067f446f2848050359f2db23df6244
leemmit <kolmykovl226@gmail.com>
2024-02-09 18:34:02 +0300

    first commit

---
+++ menu.php
@@ -0,0 +1,9 @@
+<ul class="menu">
+    <li><button data-tab="tab-1" class="title button-55" role="button">Концепция</button></li>
+    <li><button data-tab="tab-2" class="title button-55" role="button">Виды</button></li>
+    <li><button data-tab="tab-3" class="title button-55" role="button">Технологии</button></li>
+    <li><button data-tab="tab-4" class="title button-55" role="button">Заработок</button></li>
+    <li><button data-tab="tab-5" class="title button-55" role="button">Обучение</button></li>
+    <li class="sect"></li>
+    <li><button data-tab="us" class="title button-55" role="button">О Нас</button></li>
+</ul>
